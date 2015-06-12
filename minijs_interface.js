7f7c8d665e1d306c73cef702c94cacbb935d8287
kungfooman <lama12345@gmail.com>
2015-06-12 05:47:24 +0200

    Add minijs_PrettyPrintHTML

--- minijs_interface.js
+++ minijs_interface.js
@@ -7,15 +7,18 @@ Object.prototype.error = function (message, t) {
 };
 
 function minijs_print(string) {
-	document.getElementById('minijs_output').innerHTML = string.replace(/&/g, '&amp;').replace(/[<]/g, '&lt;');
+	document.getElementById('minijs_output').innerHTML += string;
 }
+function minijs_clear() {
+	document.getElementById('minijs_output').innerHTML = "";
+};
 
 function minijs_go(source) {
 	var string, tree;
 	try {
 		minijs_parser = new MiniJS_Parser();
 		tree = minijs_parser.parse(source);
-		string = JSON.stringify(tree, ['key', 'name', 'message', 'value', 'arity', 'first', 'second', 'third', 'fourth'], 4);
+		string = JSON.stringify(tree, ["id", 'key', 'name', 'message', 'value', 'arity', 'first', 'second', 'third', 'fourth'], 4);
 	} catch (e) {
 		string = JSON.stringify(e, ['name', 'message', 'from', 'to', 'key', 'value', 'arity', 'first', 'second', 'third', 'fourth'], 4);
 	}
@@ -23,7 +26,7 @@ function minijs_go(source) {
 }
 
 //minijs_go("var MiniJS_Parser = " + MiniJS_Parser.toSource() + ";");
-minijs_go(minijs_input());
+//minijs_go(minijs_input());
 
 function minijs_input() {
 	return document.getElementById('minijs_input').value;
@@ -32,3 +35,116 @@ function minijs_input() {
 function minijs_parse() {
 	minijs_go(minijs_input());
 };
+
+function minijs_prettyprint() {
+	minijs_parser = new MiniJS_Parser();
+	tree = minijs_parser.parse(minijs_input());
+	minijs_print(minijs_PrettyPrintHTML(tree, 0));
+};
+
+function minijs_PrettyPrintHTML(node, depth)
+{
+	var html = new HTML();
+
+	if (typeof node == "undefined")
+		return "null";
+
+	switch (node.id) {
+		case "statements": {
+			html.table("class=prettyprinthtml");
+			for (var i = 0; i < node.statements.length; i++) {
+				html.tr();
+				//html.td("node.statements[" + i + "]");
+				html.td("#" + i);
+				html.td(minijs_PrettyPrintHTML(node.statements[i], depth + 1));
+			}
+			return html.toString();
+		}
+	
+		case "(literal)":
+		case "(name)": {
+			return node.value;
+		}
+		
+		case "+":
+		case "-":
+		case "*":
+		case "/":
+		case "%":
+		case "^":
+		case "=": {
+			html.table("class=prettyprinthtml");
+			html.tr();
+			html.td(node.value, "colspan=2");
+			html.tr();
+			html.td(minijs_PrettyPrintHTML(node.first, depth + 1));
+			html.td(minijs_PrettyPrintHTML(node.second, depth + 1));
+			return html.toString();
+		}
+		
+		case "call": {
+			html.table("class=prettyprinthtml");
+			html.tr();
+			html.td("call");
+			html.td(node.name);
+			for (var i = 0; i < node.args.length; i++) {
+				html.tr();
+				html.td("args #" + i);
+				html.td(minijs_PrettyPrintHTML(node.args[i], depth + 1));
+			}
+			return html.toString();
+		}
+		
+		case "function": {
+			html.table("class=prettyprinthtml");
+			html.tr();
+			html.td("function");
+			html.td(node.name);
+			for (var i = 0; i < node.args.length; i++) {
+				html.tr();
+				html.td("args #" + i);
+				html.td(node.args[i].value);
+			}
+			html.tr();
+			html.td(minijs_PrettyPrintHTML(node.value, depth + 1), "colspan=2");
+			return html.toString();
+		}
+		
+		case "identifier": {
+			html.table("class=prettyprinthtml");
+			html.tr();
+			//html.td("name");
+			html.td(node.value);
+			return html.toString();
+		}
+		
+		default:
+			//console.log("default biatch");
+			//return "default: " + node.value + " " + node.arity;
+		
+			html.add("<div style='text-align: left'>");
+		
+			nonewline = function(msg) { return msg.replace(/\r\n/g, " ").replace(/\n/g, " "); }
+			
+			for (key in node) {
+				if (key == "args" || key == "left" || key == "right" || key == "symbol") // will be printed separately
+					continue;
+				html.add("<b>node." + key + "</b>=<b>" + node[key] + "</b> ");
+			}
+			//print((indent + nonewline(tmp));
+			if (typeof node.symbol != "undefined") {
+				//print((indent + "node.symbol:");
+				
+				for (key in node.symbol) {
+					if (key == "args" || key == "left" || key == "right" || key == "symbol") // will be printed separately
+						continue;
+					html.add("<b>node." + key + "</b>=<b>" + node[key] + "</b> ");
+				}
+				//print((indent + nonewline(tmp));
+				
+			}
+			html.add("</div>");
+			
+			return html.toString();
+	}
+}
