ccdab54e70086a78bf30ddf3c608af74e2aed6de
kungfooman <lama12345@gmail.com>
2015-06-13 04:18:11 +0200

    Unify MiniJS code into MiniJS.js

--- minijs_interface.js
+++ minijs_interface.js
@@ -16,7 +16,7 @@ function minijs_clear() {
 function minijs_go(source) {
 	var string, tree;
 	try {
-		minijs_parser = new MiniJS_Parser();
+		minijs_parser = new MiniJS.Parser();
 		tree = minijs_parser.parse(source);
 		string = JSON.stringify(tree, ["id", 'key', 'name', 'message', 'value', 'arity', 'first', 'second', 'third', 'fourth'], 4);
 	} catch (e) {
@@ -38,7 +38,7 @@ function minijs_parse() {
 
 function minijs_prettyprint() {
 	try {
-		minijs_parser = new MiniJS_Parser();
+		minijs_parser = new MiniJS.Parser();
 		tree = minijs_parser.parse(minijs_input());
 		if (typeof tree.length != "undefined")
 		{
@@ -47,135 +47,9 @@ function minijs_prettyprint() {
 				statements: tree			
 			}
 		}
-		minijs_print(minijs_PrettyPrintHTML(tree, 0));
+		minijs_print(MiniJS.PrettyPrintHTML(tree, 0));
 	} catch (e) {
 		console.log("Exception: ", e);
 		
 	}
 };
-
-function minijs_PrettyPrintHTML(node, depth)
-{
-	var html = new HTML();
-
-	if (typeof node == "undefined")
-		return "null";
-
-	// rewrite an array as statements-node
-	if (typeof node.length != "undefined") {
-		console.log(node)
-		node = {
-			id: "statements",
-			statements: node
-		}
-	}
-	
-	switch (node.id) {
-		case "statements": {
-			html.table("class=prettyprinthtml");
-			html.tr().td("statements", "colspan=2");
-			for (var i = 0; i < node.statements.length; i++) {
-				html.tr();
-				//html.td("node.statements[" + i + "]");
-				html.td("#" + i);
-				html.td(minijs_PrettyPrintHTML(node.statements[i], depth + 1));
-			}
-			return html.toString();
-		}
-	
-		case "(literal)":
-		case "(name)": {
-			return node.value;
-		}
-		
-		case "+":
-		case "-":
-		case "*":
-		case "/":
-		case "%":
-		case "^":
-		case "=":
-		case "while": {
-			html.table("class=prettyprinthtml");
-			html.tr();
-			html.td(node.value, "colspan=2");
-			html.tr();
-			html.td(minijs_PrettyPrintHTML(node.first, depth + 1));
-			html.td(minijs_PrettyPrintHTML(node.second, depth + 1));
-			return html.toString();
-		}
-		
-		case "call": {
-			html.table("class=prettyprinthtml");
-			html.tr();
-			html.td("call");
-			html.td(node.id);
-			for (var i = 0; i < node.args.length; i++) {
-				html.tr();
-				html.td("args #" + i);
-				html.td(minijs_PrettyPrintHTML(node.args[i], depth + 1));
-			}
-			return html.toString();
-		}
-		
-		case "function": {
-			html.table("class=prettyprinthtml");
-			html.tr();
-			//html.td("function");
-			html.td(node.id, "colspan=2");
-			for (var i = 0; i < node.first.length; i++) {
-				html.tr();
-				html.td("args #" + i);
-				html.td(node.first[i].value);
-			}
-			html.tr();
-			html.td(minijs_PrettyPrintHTML(node.second, depth + 1), "colspan=2");
-			return html.toString();
-		}
-		
-		case "identifier": {
-			html.table("class=prettyprinthtml");
-			html.tr();
-			//html.td("name");
-			html.td(node.value);
-			return html.toString();
-		}
-		case "return": {
-			html.table("class=prettyprinthtml");
-			html.tr();
-			html.td("return");
-			html.tr();
-			html.td(minijs_PrettyPrintHTML(node.first, depth + 1));
-			return html.toString();
-		}
-		
-		default:
-			//console.log("default biatch");
-			//return "default: " + node.value + " " + node.arity;
-		
-			html.add("<div style='text-align: left'>");
-		
-			nonewline = function(msg) { return msg.replace(/\r\n/g, " ").replace(/\n/g, " "); }
-			
-			for (key in node) {
-				if (key == "args" || key == "left" || key == "right" || key == "symbol") // will be printed separately
-					continue;
-				html.add("<b>node." + key + "</b>=<b>" + node[key] + "</b> ");
-			}
-			//print((indent + nonewline(tmp));
-			if (typeof node.symbol != "undefined") {
-				//print((indent + "node.symbol:");
-				
-				for (key in node.symbol) {
-					if (key == "args" || key == "left" || key == "right" || key == "symbol") // will be printed separately
-						continue;
-					html.add("<b>node." + key + "</b>=<b>" + node[key] + "</b> ");
-				}
-				//print((indent + nonewline(tmp));
-				
-			}
-			html.add("</div>");
-			
-			return html.toString();
-	}
-}
